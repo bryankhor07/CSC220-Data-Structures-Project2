5b2c02ba0d3522b20dd210ef4815bdb5e81a580a
Bryan Khor <67664138+bryankhor07@users.noreply.github.com>
2023-07-06 20:53:39 -0700

    Create StackInterface.java

---
+++ StackInterface.java
@@ -0,0 +1,24 @@
+/*
+ * **********************************************
+ * San Francisco State University
+ * CSC 220 -  Data Structures
+ * File Name: StackInterface.java
+ * Author: Frank M. Carrano
+ * Author: Timothy M. Henry
+ * Author: Duc Ta
+ * Author: Bryan Khor
+ * **********************************************
+ */
+
+// Please DO NOT CHANGE this file.
+
+package assignment03PartB;
+
+public interface StackInterface<T>
+{
+    public void push(T newEntry);
+    public T pop();
+    public T peek();
+    public boolean isEmpty();
+    public void clear();
+}
